e1feafe797f429f0d9a01fa439b733d90caf487d
Dmitry <danev@htr.su>
2018-12-01 23:29:08 +0300

    Docs improvements

--- README.md
+++ README.md
@@ -1,7 +1,73 @@
-# loop-limiter
-A small utilities library to provide loop scheduling
+# Time Limiter
+A small utility class to help to limit script execution time.
 
-Copyright © 2000 Morjodrom <metaldr@yandex.ru>
-This work is free. You can redistribute it and/or modify it under the
-terms of the **Do What The Fuck You Want To Public License**, Version 2,
-as published by Sam Hocevar. See the _license_ file for more details.
+# Basic usage
+The class is usually used within loops with heavy time consumption.
+
+```php
+<?php
+// maximum time to execute one request
+$REQUEST_TIMEOUT_SEC = 5; 
+
+$curlClient = new SomeCurlClient(); // just an example.
+
+// shared hosting often has a limit. E.g. 30 seconds
+$maxExecutionTime = ini_get('max_execution_time'); 
+$timeLimiter = new \timelimiter\TimeLimiter($maxExecutionTime, $REQUEST_TIMEOUT_SEC);
+
+// check if there is time left to prevent
+// the script fail with 504 timeout
+while($timeLimiter->valid()){ 
+    $result = $curlClient->doSomeHeavyJob([
+        'timeout' => $REQUEST_TIMEOUT_SEC
+    ]);
+    // handle the result
+    // ...
+}
+```
+
+# Installation
+As a production dependency using [Composer](https://getcomposer.org/):
+
+    composer require morjodrom/time-limiter
+
+If you only need this class during development, you should add it as a dev-dependency:
+
+    composer require --dev morjodrom/time-limiter
+
+# Constructor options
+``int $limitSeconds`` - seconds to process. 0 equals to INF.
+Can be used with ``ini_get('max_execution_time')`` for the first parameter.
+<b>Note!</b> You can freely pass any time just to limit the script execution
+e.g. to send progress data to the client.
+
+``[int $preliminaryTimeout] = DEFAULT_TIME_UP_SECONDS = 3`` - seconds to stop preliminary before the timeout.
+``$preliminaryTimeout`` must be a bit greater that the longest operation you perform in a loop
+to prevent script fatal error with a timeout. E.g, a typical share hosting limits:
+- ``max_execution_time`` is 30 seconds
+- a heavy operation is a curl request with 5 seconds timeout
+- script beginning and ending may take some time less than 1 second
+
+**So, the total time (in seconds) you have for execution is roughly:**
+
+30 > 0.5 + 5n + 0.5
+
+There is only 5.5 seconds left on some N iteration of curl and you risk to have a fatal error
+if a next operation starts. So, we have to stop heavy code preliminary before the max execution
+timeout:
+
+30 > 0.5 + 5n + preliminaryTimeout(5) + 0.5
+
+Having around 5.5 seconds reserved the script ends safely.
+
+* ``[int|null $startTimestamp = $_SERVER['REQUEST_TIME']`` is used by default. Must be a timestamp
+since Unix Epoch (January 1 1970 00:00:00 GMT).
+
+# Methods
+``current(): int`` - returns number of seconds left before the timeout 
+``valid(): bool`` - returns if there is time left to perform the script safely
+
+# Support
+Feel free to open an issue: <https://github.com/Morjodrom/time-limiter/issues>
+
+# License - MIT
--- lib/TimeLimiter.php
+++ lib/TimeLimiter.php
@@ -5,6 +5,8 @@ namespace timelimiter;
 /**
  * Class TimeLimiter
  *
+ * A small utility class to help to limit script execution time.
+ *
  * @package timelimiter
  * @author Morjodrom
  */
@@ -24,22 +26,52 @@ class TimeLimiter
     /**
      * TimeLimiter constructor.
      *
-     * Can be used with ``ini_get('max_execution_time')`` for the first parameter.
      *
-     * @param int      $limitSeconds - seconds to process. 0 equals to INF
-     * @param int      $timeUpSeconds - seconds to stop preliminary before the timeout
-     * @param int|null $startTimestamp - $_SERVER['REQUEST_TIME'] is used by default
+     * @param int      $limitSeconds - seconds to process. 0 equals to INF.
+     * Can be used with ``ini_get('max_execution_time')`` for the first parameter.
+     * <b>Note!</b> You can freely pass any time just to limit the script execution
+     * e.g. to send progress data to the client.
+     * @param int      $preliminaryTimeout - seconds to stop preliminary before the timeout.
+     * ``$preliminaryTimeout`` must be a bit greater that the longest operation you perform in a loop
+     * to prevent script fatal error with a timeout. E.g, a typical share hosting limits:
+     * - max_execution_time is 30 seconds
+     * - a heavy operation is a curl request with 5 seconds timeout
+     * - script beginning and ending may take some time less than 1 second
+     * So, the total time you have for execution is roughly:
+     * 30 > 0.5 + 5n + 0.5
+     * There is only 5.5 seconds left on some N iteration of curl and you risk to have a fatal error
+     * if a next operation starts. So, we have to stop heavy code preliminary before the max execution
+     * timeout:
+     * 30 > 0.5 + 5n + preliminaryTimeout(5) + 0.5
+     * @param int|null $startTimestamp - $_SERVER['REQUEST_TIME'] is used by default. Must be a timestamp
+     * since Unix Epoch (January 1 1970 00:00:00 GMT).
+     *
+     *
+     * @example
+     * $REQUEST_TIMEOUT_SEC = 5; // maximum time to execute one request
+     *
+     * $curlClient = new SomeCurlClient();
+     * $maxExecutionTime = ini_get('max_execution_time'); // shared hosting often has a limit. E.g. 30 seconds
+     * $timeLimiter = new \timelimiter\TimeLimiter($maxExecutionTime, $REQUEST_TIMEOUT_SEC);
+     *
+     * while($timeLimiter->valid()){ // check if there is time left to prevent the script fail with 504 timeout
+     * $result = $curlClient->doSomeHeavyJob([
+     * 'timeout' => $REQUEST_TIMEOUT_SEC
+     * ]);
+     * // handle the result
+     * // ...
+     * }
      *
      * @see INF
      */
-    public function __construct($limitSeconds, $timeUpSeconds = self::DEFAULT_TIME_UP_SECONDS, $startTimestamp = null) {
+    public function __construct($limitSeconds, $preliminaryTimeout = self::DEFAULT_TIME_UP_SECONDS, $startTimestamp = null) {
 
         if($limitSeconds === 0) {
             $this->timeout = INF;
         }
         else {
             $startTimestamp = $startTimestamp ?: (int) $_SERVER['REQUEST_TIME'];
-            $this->timeout = $startTimestamp - $timeUpSeconds + $limitSeconds;
+            $this->timeout = $startTimestamp - $preliminaryTimeout + $limitSeconds;
         }
     }
 
