5c92d5ec8b7d12c774f47e91a28768529aaa45ad
Dmitry <metaldr@yandex.ru>
2024-08-20 15:07:37 +0300

    Clarify the intentions of limiter options, remove unnecessary and bulky doc comment.

--- lib/TimeLimiter.php
+++ lib/TimeLimiter.php
@@ -36,49 +36,17 @@ class TimeLimiter implements \Iterator
     /**
      * TimeLimiter constructor.
      *
-     *
-     * @param int $limitSeconds - seconds to process. 0 equals to INF.
-     * Can be used with ``ini_get('max_execution_time')`` for the first parameter.
-     * <b>Note!</b> You can freely pass any time just to limit the script execution
-     * e.g. to send progress data to the client.
-     * @param int $preliminaryTimeout - seconds to stop preliminary before the timeout.
-     * ``$preliminaryTimeout`` must be a bit greater that the longest operation you perform in a loop
-     * to prevent script fatal error with a timeout. E.g, a typical share hosting limits:
-     * - max_execution_time is 30 seconds
-     * - a heavy operation is a curl request with 5 seconds timeout
-     * - script beginning and ending may take some time less than 1 second
-     * So, the total time you have for execution is roughly:
-     * 30 > 0.5 + 5n + 0.5
-     * There is only 5.5 seconds left on some N iteration of curl and you risk to have a fatal error
-     * if a next operation starts. So, we have to stop heavy code preliminary before the max execution
-     * timeout:
-     * 30 > 0.5 + 5n + preliminaryTimeout(5) + 0.5
-     * @param int|null $startTimestamp - $_SERVER['REQUEST_TIME'] is used by default. Must be a timestamp
-     * since Unix Epoch (January 1 1970 00:00:00 GMT).
-     *
-     *
-     * @example
-     * $REQUEST_TIMEOUT_SEC = 5; // maximum time to execute one request
-     *
-     * $curlClient = new SomeCurlClient();
-     * $maxExecutionTime = ini_get('max_execution_time'); // shared hosting often has a limit. E.g. 30 seconds
-     * $timeLimiter = new \timelimiter\TimeLimiter($maxExecutionTime, $REQUEST_TIMEOUT_SEC);
-     *
-     * while($timeLimiter->valid()){ // check if there is time left to prevent the script fail with 504 timeout
-     * $result = $curlClient->doSomeHeavyJob([
-     * 'timeout' => $REQUEST_TIMEOUT_SEC
-     * ]);
-     * // handle the result
-     * // ...
-     * }
+     * @param int $maxExecutionTimeSec - seconds to process. 0 equals to INF. Can be used with ``ini_get('max_execution_time')``.
+     * @param int $iterationMaxDurationS - longest possible duration of an iteration to prevent risk of a shutdown during the last iteration.
+     * @param int|null $startTimestamp - timestamp of the iteration start. $_SERVER['REQUEST_TIME'] is used by default.
      *
      * @see INF
      */
-    public function __construct(int $limitSeconds = INF, int $preliminaryTimeout = self::DEFAULT_TIME_UP_SECONDS, int $startTimestamp = null)
+    public function __construct(int $maxExecutionTimeSec = INF, int $iterationMaxDurationS = self::DEFAULT_TIME_UP_SECONDS, int $startTimestamp = null)
     {
         $this->startTimestamp = $startTimestamp ?: $_SERVER['REQUEST_TIME'];
-        $this->preliminaryTimeout = $preliminaryTimeout;
-        $this->limitSeconds = $limitSeconds > 0 ? $limitSeconds : INF;
+        $this->preliminaryTimeout = $iterationMaxDurationS;
+        $this->limitSeconds = $maxExecutionTimeSec > 0 ? $maxExecutionTimeSec : INF;
     }
 
 
